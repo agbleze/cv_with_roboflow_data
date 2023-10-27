d657bf29b178c4897803e062df00ecf83b6d23bb
agbleze <lagbleze@st.ug.edu.gh>
2023-10-27 23:47:56 +0200

    Update cluster_img.py

--- cluster_img.py
+++ cluster_img.py
@@ -14,9 +14,22 @@ train_img = "/Users/lin/Documents/python_venvs/cv_with_roboflow_data/Tomato-pest
 train_raw = cl.import_data(train_img)
 
 
-#%%
+#%% extract features
 
 train_feat = cl.extract_feat(train_raw)
+
+#%% embedding using tSNE
+xycoord = cl.embedding(train_feat)
+
+#%%
+labels = cl.cluster()
+
+
+
+
+
+
+
 
 
 
