f4ae9c85921b17d9ef8d5b8f2b2560be9f29258c
agbleze <lagbleze@st.ug.edu.gh>
2023-10-27 23:42:05 +0200

    Update cluster_img.py

--- cluster_img.py
+++ cluster_img.py
@@ -1,5 +1,23 @@
 
 #%%
-import clustimage
+from clustimage import Clustimage
 
 
+
+# %%
+
+cl = Clustimage(method="pca")
+
+#%%
+train_img = "/Users/lin/Documents/python_venvs/cv_with_roboflow_data/Tomato-pest&diseases-1/train"
+
+train_raw = cl.import_data(train_img)
+
+
+#%%
+
+train_feat = cl.extract_feat(train_raw)
+
+
+
+# %%
