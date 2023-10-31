c847b92d69cd9dda9213468b26a7d801db3c05b5
agbleze <lagbleze@st.ug.edu.gh>
2023-10-31 17:03:16 +0100

    Update cluster_app.py

--- cluster_app.py
+++ cluster_app.py
@@ -16,6 +16,45 @@ from dash import Dash, dcc, html, Input, Output, State, callback, callback_conte
 import os
 import datetime
 from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+import base64
+import dash_ag_grid as dag
+#from dataclasses import dataclass
+from image_cluster_creator import UploadedFileUtil, ImageClusterCreator
+
+
+# @dataclass
+# class FolderReturns:
+#     uploaded_file_name: str
+#     img_folder: str
+#     img_list: str
+    
+# class UploadedFileUtil:
+#     def __init__(self, extract_folder_name="extract_folder"):
+#         self.extract_folder_name = extract_folder_name
+   
+#     @lru_cache(maxsize=None)
+#     def unzip_upload(self, filenames_list):
+        
+#         zip_file_1 = filenames_list[0]
+#         with ZipFile(zip_file_1, "r") as file:
+#                 file.extractall(self.extract_folder_name)
+                    
+#     def get_upload_paths_names(self, contents_list, filenames_list, 
+#                                img_ext: str = ".jpg"
+#                         ):
+#         uploaded_file_name = filenames_list[0].split(".")[0]
+#         img_path = os.path.join(self.extract_folder_name, uploaded_file_name)
+#         imgs_path_list = glob(f"{img_path}/*{img_ext}")
+#         return FolderReturns(uploaded_file_name=uploaded_file_name,
+#                              img_folder=img_path,img_list=imgs_path_list
+#                              )
+
+
+uploaded_file_util = UploadedFileUtil(extract_folder_name="subset_extract_folder")
+
+img_cluster = ImageClusterCreator()
 
 
 
@@ -48,7 +87,8 @@ def create_upload_button():
     return upload_button
 
 img_page = dbc.Row([
-                    dbc.Col([html.Div(id='output-image-upload'),
+                    dbc.Col([html.H5(id="detect-click"),
+                            html.Div(id='output-image-upload'),
                              html.Br(),
                              html.Img(id='bar-graph-matplotlib')
                             ], 
@@ -106,34 +146,55 @@ app.layout = main_page  #html.Div("Image clustering app")
               )
 def update_output(list_of_contents, list_of_names):
     if list_of_contents is not None:
+        uploaded_file_util.unzip_upload(filenames_list=list_of_names)
+        upload_paths = uploaded_file_util.get_upload_paths_names()
+        imgs_list = upload_paths.img_list
+        filename = upload_paths.uploaded_file_name
+        img_1 = imgs_list[0]
+        img_1_opened = Image.open(img_1)
+        
         #contents_test = "valid.zip"
-        folder_name = list_of_names[0].split(".")[0]
-        with ZipFile(list_of_names[0], "r") as file:
-                extract_folder = "extract_folder"
-                file.extractall(extract_folder)
+        # folder_name = list_of_names[0].split(".")[0]
+        # with ZipFile(list_of_names[0], "r") as file:
+        #         extract_folder = "extract_folder"
+        #         file.extractall(extract_folder)
                 
-        img_folder = os.path.join(extract_folder, folder_name)
+        # img_folder = os.path.join(extract_folder, folder_name)
                 
-        img = glob(f"{img_folder}/*.jpg")[0]
-        pil_image = Image.open(img)
-        children = [html.H5(list_of_names[0]),
+        # img = glob(f"{img_folder}/*.jpg")[0]
+        
+        #pil_image = Image.open(img)
+        children = [html.H5(filename),
                     html.Br(),
-                   html.Img(src=pil_image)
+                   html.Img(src=img_1_opened)
                     ]
         return children
     
     
-@callback(Output(component_id="bar-graph-matplotlib", component_property="src"),
+@callback(#Output(component_id="bar-graph-matplotlib", component_property="src"),
+          Output(component_id="detect-click", component_property="children"),
           Input(component_id="id_cluster_button", component_property="n_clicks"),
-          Input('upload-image', 'contents'),
-            State('upload-image', 'filename')
+        #  Input('upload-image', 'contents'),
+        #State('upload-image', 'filename')
           )
-def show_cluster_img(cluster_button_clicked, contents_uploaded, upload_filenames):
-    if cluster_button_clicked:
-        folder_name = upload_filenames[0].split(".")[0]
+def dezrmine_click(n_clicks):
+    if n_clicks:
+        img_folder_path = uploaded_file_util.img_path
+        img_cluster.extract_img_features(img_folder_path=img_folder_path)
+        #fig_clustered_imgs = img_cluster.plot_clustered_imgs()
+        #buf = BytesIO()
+        #fig_clustered_imgs.savefig(buf, format="png")
+        #encoded_fig = base64.b64encode(buf.getbuffer()).decode("ascii")
+        #fig_img = f"data:image/png;base64,{encoded_fig}"
+        return uploaded_file_util.img_path
+# def show_cluster_img(cluster_button_clicked, contents_uploaded, upload_filenames):
+#     if cluster_button_clicked:
+#         folder_name = upload_filenames[0].split(".")[0]
+        
+        
         
     
 
 if __name__ == "__main__":
-    app.run(port=8010, debug=True, use_reloader=True)
+    app.run(port=8010, debug=False, use_reloader=True)
 
