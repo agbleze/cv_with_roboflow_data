9f265775952d77b46edabd5c09d4f8b8f92cbcfa
linus <lagbleze@st.ug.edu.gh>
2024-08-16 08:06:13 +0200

    prune dataset

--- prune_dataset.py
+++ prune_dataset.py
@@ -3,6 +3,11 @@
 #%%
 from datumaro.components.dataset import Dataset
 from datumaro.components.environment import Environment
-from datumaro.components.prune import prune
+from datumaro.components.algorithms.hash_key_inference.prune import prune
 
+img_dir = "/home/lin/codebase/__cv_with_roboflow_data/field_crop_with_disease"
 
+env = Environment()
+detected_format = env.detect_dataset(path=img_dir)
+
+# %%
