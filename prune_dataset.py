4e1ae5726039d0bfe6ba044cd5174a98359ea56c
linus <lagbleze@st.ug.edu.gh>
2024-08-16 22:56:23 +0200

    added prune_dataset.py

--- prune_dataset.py
+++ prune_dataset.py
@@ -3,11 +3,20 @@
 #%%
 from datumaro.components.dataset import Dataset
 from datumaro.components.environment import Environment
-from datumaro.components.algorithms.hash_key_inference.prune import prune
+from datumaro.components.algorithms.hash_key_inference.prune import Prune
 
 img_dir = "/home/lin/codebase/__cv_with_roboflow_data/field_crop_with_disease"
 
 env = Environment()
 detected_format = env.detect_dataset(path=img_dir)
+
+# %%
+
+dataset = Dataset.import_from(img_dir, detected_format[0])
+prune = Prune(dataset, cluster_method="entropy")
+
+
+#%%
+prune.get_pruned()
 
 # %%
