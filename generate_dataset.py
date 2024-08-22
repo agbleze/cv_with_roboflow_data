b1d01eda0d1e8a6b123c373541c0404f6c0e1c2e
linus <lagbleze@st.ug.edu.gh>
2024-08-22 22:06:41 +0200

    added link for download of colorizer model and others

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,6 +5,7 @@ from datumaro.plugins.synthetic_data import FractalImageGenerator
 
 
 # %%
+# link for model download https://storage.openvinotoolkit.org/repositories/datumaro/models/colorization/
 fragen = FractalImageGenerator(output_dir="gendata", count=100, shape=(700,700)).generate_dataset()
 # %%
 fragen.generate_dataset()
