8267e524e0f9c7ee9abb94c99871b4715b555de5
linus <lagbleze@st.ug.edu.gh>
2024-08-23 22:59:25 +0200

    add generate_dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,7 +6,16 @@ from datumaro.plugins.synthetic_data import FractalImageGenerator
 
 # %%
 # link for model download https://storage.openvinotoolkit.org/repositories/datumaro/models/colorization/
-fragen = FractalImageGenerator(output_dir="gendata", count=100, shape=(700,700)).generate_dataset()
+fragen = FractalImageGenerator(output_dir="gendata", count=100, shape=(700,700))#.generate_dataset()
 # %%
-fragen.generate_dataset()
+res = fragen.generate_dataset()
 # %%
+
+"""
+TODO:
+1. incorporate new link for colorouer model and other assets 
+into datumaro package
+
+
+use new link 
+"""
