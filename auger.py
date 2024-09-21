e7fb43047b85b51aa7945502dae86eb98a737d8f
linus <lagbleze@st.ug.edu.gh>
2024-09-21 23:54:38 +0200

    updated

--- auger.py
+++ auger.py
@@ -49,17 +49,24 @@ def is_valid_albumentation_parameter(augconfig):
         else:
             return True
             
-def compose_albumentation_pipeline(augconfig):
+def compose_albumentation_pipeline(augconfig, replay=False):
     is_valid_albumentation_parameter(augconfig=augconfig)
     pipeline = []
     for augtype, aug_params in augconfig.items():
         aug_func = getattr(A, augtype)
         pipeline.append(aug_func(**aug_params))
-    return A.Compose(pipeline, 
-                     bbox_params=A.BboxParams(format="coco", 
-                                              label_fields=["class_labels", "class_categories"]
-                                              )
-                     )
+    if not replay:
+        return A.Compose(pipeline, 
+                        bbox_params=A.BboxParams(format="coco", 
+                                                label_fields=["class_labels", "class_categories"]
+                                                )
+                        )
+    else:
+        return A.ReplayCompose(transforms=pipeline,
+                                bbox_params=A.BboxParams(format="coco", 
+                                                        label_fields=["class_labels", "class_categories"]
+                                                        )
+                                )
 
 #%%
 coco_path = "/home/lin/codebase/cv_with_roboflow_data/coco_annotation_coco.json"
@@ -104,12 +111,6 @@ cat = [ann["category_id"] for ann in anns]
 
 
 #%%
-for ann in anns:
-    coco.annToMask(ann["segmentation"])
-    break
-
-
-#%%
 albu_compose = compose_albumentation_pipeline(augconfig=augconfig)
 
 
@@ -119,10 +120,23 @@ transformed = albu_compose(image=img, bboxes=bbox,
              class_categories=["ripe", "ripe", "ripe"]
              )
 
+
 #%%
 
-transformed["image"] 
+#Image.open(img_path)
 
+albu_compose(image=Image.open(img_path), bboxes=bbox, 
+             masks=segms, class_labels=cat, 
+             class_categories=["ripe", "ripe", "ripe"]
+             )
+
+#%%
+
+augimg = transformed["image"] 
+
+#%%
+
+augimg.save()
 #%%
 transformed["bboxes"] 
 
@@ -198,7 +212,7 @@ from torchvision.io import read_image
 import torchvision.transforms.functional as F
 import torch
 import matplotlib.pyplot as plt
-
+from PIL import Image
 #%%
 image = read_image(img_path)
 
@@ -227,4 +241,200 @@ plt.show()
 # %% create function to take aug, visualize default img and augmentd img with mask and bbox
 
 def augment_and_visualize(augconfig, img_path):
+    albu_compose = compose_albumentation_pipeline(augconfig=augconfig)
+    img, anns = get_image_and_annotations(coco=coco, img_id=1)
+    img_name = img["file_name"]
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
+    #for ann in anns:
+    segms = [coco.annToMask(ann) for ann in anns]
+    bbox = [ann["bbox"] for ann in anns]
+    cat = [ann["category_id"] for ann in anns]
+    bbox_converted = [[bb[0], bb[1], bb[0] + bb[2], bb[1] + bb[3]] for bb in bbox]
+    bbox_converted = torch.tensor(bbox_converted, dtype=torch.float)
+    segms_tensor = torch.tensor(segms, dtype=torch.bool)
+    
+    transformed = albu_compose(image=img, bboxes=bbox, 
+                                masks=segms, class_labels=cat, 
+                                class_categories=["ripe", "ripe", "ripe"]
+                                )
+    augmented_image = transformed["image"] 
+    filepath = "augmented_img.png"
+    Image.fromarray(augmented_image).save(filepath)
+    augmented_bbox = transformed["bboxes"] 
+    augmented_masks = transformed["masks"]
+    #F.to_pil_image(augmented_image)
+    #augmented_image = F.to_tensor(F.to_pil_image(augmented_image)).to(torch.uint8)
+    augmented_bbox_converted = [[bb[0], bb[1], bb[0] + bb[2], bb[1] + bb[3]] for bb in augmented_bbox]
+    augmented_bbox_converted = torch.tensor(augmented_bbox_converted, dtype=torch.float)
+    augmented_segms = torch.tensor(augmented_masks, dtype=torch.bool)
+    
+    image = read_image(img_path)
+    augmented_image = read_image(filepath)
+    
+    images_with_bboxes = draw_bounding_boxes(image=image, boxes=bbox_converted,
+                                            colors="red", width=2
+                                            )
+    image_with_mask = draw_segmentation_masks(image=images_with_bboxes, 
+                                              masks=segms_tensor, 
+                                              alpha=0.9
+                                              )
+    
+    
+    
+    augmentedimages_with_bboxes = draw_bounding_boxes(image=augmented_image, 
+                                                      boxes=augmented_bbox_converted,
+                                                        colors="red", width=2
+                                                        )
+    augmented_img_with_masks = draw_segmentation_masks(image=augmentedimages_with_bboxes,
+                                                       masks=augmented_segms
+                                                       )
+    plt.imshow(F.to_pil_image(image_with_mask))
+    plt.axis("off")
+    plt.title("Original Image")
+    plt.show()
+    #Image
+    #torch.asarray(augmented_img_with_masks)
+    #print(augmented_img_with_masks.reshape((1,2,0)))
+    augshape = augmented_img_with_masks.shape
+    plt.imshow(F.to_pil_image(augmented_img_with_masks))
+    #plt.imshow(augmented_img_with_masks.transpose(1,2,0))
+    plt.axis("off")
+    plt.title("Augmented Image")
+    plt.show()
+    
+#%%
+augment_and_visualize(augconfig=augconfig, img_path=img_path)   
+    
+#%%  TODO: use of ReplayCompose to record, serialize aug used
+# for each image save the aug used wuth Replay
+
+#%%
+from glob import glob
+import os
+def augment_imgs(augconfig, img_dir, coco_annfilepath):
+    coco = COCO(coco_annfilepath)
+    imglist = glob(f"{img_dir}/*")
+    for img_path in imglist:
+        imgname = os.path.basename(img_path)
+        imgid = [coco_obj["id"] for coco_obj in coco.imgs if 
+                 os.path.basename(coco_obj["file_name"]) == imgname][0]
+        ann_ids = coco.getAnnIds(imgIds=imgid)
+        anns = coco.loadAnns(ids=ann_ids)
+        segmasks = [ann["segmentation"] for ann in anns]
+        all_annid = [ann["id"] for ann in anns]
+        ann_category_ids = [ann["category_id"] for ann in anns]
+        bboxes = [ann["bboxes"] for ann in anns]
+        augpipeline = compose_albumentation_pipeline(augconfig=augconfig)
+        imgread = cv2.imread(img_path)
+        image = cv2.cvtColor(src=imgread, code=cv2.COLOR_BGR2RGB)
+        augmented_obj = augpipeline(image=image, bboxes=bboxes, masks=segmasks, 
+                                    category_ids=ann_category_ids
+                                    )
+        aug_image = augmented_obj["image"]
+        aug_bboxes = augmented_obj["bboxes"]
+        aug_masks = augmented_obj["masks"]
+        
+        
+        
+        
+    
+   
+    
+    
+    
+
+
+# %%
+import numpy as np
+from pycocotools import mask as coco_mask
+from skimage import measure
+
+def mask_to_polygon(binary_mask):
+    # Ensure the mask is in Fortran order
+    fortran_mask = np.asfortranarray(binary_mask)
+    encoded_mask = coco_mask.encode(fortran_mask)
+    contours = measure.find_contours(binary_mask, 0.5)
+
+    polygons = []
+    for contour in contours:
+        contour = np.flip(contour, axis=1)
+        segmentation = contour.ravel().tolist()
+        polygons.append(segmentation)
+
+    return polygons
+
+# Example usage
+binary_mask = np.array([[0, 0, 0, 0, 0],
+                         [0, 1, 1, 1, 0],
+                         [0, 1, 1, 1, 0],
+                         [0, 1, 1, 1, 0],
+                         [0, 0, 0, 0, 0]], dtype=np.uint8)
+
+polygons = mask_to_polygon(binary_mask)
+print(polygons)
+
+# %%
+# Import Supervision
+import supervision as sv
+
+# Import mask data (optional if you have raw mask data)
+#detections = sv.Detections.from_inference(prediction)
+
+# Convert each mask to a polygon
+polygons = [sv.mask_to_polygons(binary_mask)]
+# for raw mask data: polygons = sv.mask_to_polygons(mask)
+print(polygons)
+
+# %%
+from PIL import Image, ImageOps, ImageFilter
+import numpy as np
+import rembg
+import io
+
+def change_background_to_white(input_path, output_path):
+    # Remove the background
+    with open(input_path, "rb") as input_file:
+        input_data = input_file.read()
+        output_data = rembg.remove(input_data)
+
+    # Open the image with the removed background
+    img = Image.open(io.BytesIO(output_data)).convert("RGBA")
+
+    # Create a white background image
+    white_bg = Image.new("RGBA", img.size, (255, 255, 255, 255))
+
+    # Composite the image with the white background
+    final_img = Image.alpha_composite(white_bg, img)
+
+    # Apply Gaussian blur to smooth the edges
+    final_img = final_img.filter(ImageFilter.GaussianBlur(radius=2))
+
+    # Convert to RGB and save the image
+    final_img = final_img.convert("RGB")
+    final_img.save(output_path, format='JPEG')
+# Example usage
+input_path = '/home/lin/codebase/cv_with_roboflow_data/1726908281123.jpg'
+output_path = '/home/lin/codebase/cv_with_roboflow_data/output_image.jpg'
+change_background_to_white(input_path, output_path)
+
+# %%
+from PIL import Image
+
+def format_photo(input_path, output_path):
+    # Open the image file
+    img = Image.open(input_path)
+
+    # Resize the image to 600x600 pixels
+    img = img.resize((600, 600), Image.LANCZOS)
+
+    # Save the formatted image
+    img.save(output_path, format='JPEG')
+
+
+format_photo(input_path, "reduced_image.jpg")
+
+# %%
+Image.open(input_path).resize((600, 600), Image.LANCZOS).save("reduced_image.jpg")
+# %%
