071d6005cd8c40a0869c8da443ad399e466aef4d
linus <lagbleze@st.ug.edu.gh>
2024-09-23 01:40:32 +0200

    correct unmatched bboxes and masks

--- auger.py
+++ auger.py
@@ -53,7 +53,14 @@ def compose_albumentation_pipeline(augconfig, replay=False,
                                    label_fields=[#"class_labels", 
                                                  "category_ids", 
                                                  "ann_ids"
-                                                 ]
+                                                 ],
+                                   clip=True,
+                                   min_area=0,
+                                   min_visibility=0,
+                                   min_width=0,
+                                   min_height=0,
+                                   check_each_transform=True,
+                                  
                                    ):
     is_valid_albumentation_parameter(augconfig=augconfig)
     pipeline = []
@@ -63,13 +70,25 @@ def compose_albumentation_pipeline(augconfig, replay=False,
     if not replay:
         return A.Compose(pipeline, 
                         bbox_params=A.BboxParams(format="coco", 
-                                                label_fields=label_fields
+                                                label_fields=label_fields,
+                                                clip=clip,
+                                                min_area=min_area,
+                                                min_visibility=min_visibility,
+                                                min_width=min_width,
+                                                min_height=min_height,
+                                                check_each_transform=check_each_transform,
                                                 )
                         )
     else:
         return A.ReplayCompose(transforms=pipeline,
                                 bbox_params=A.BboxParams(format="coco", 
-                                                        label_fields=label_fields
+                                                        label_fields=label_fields,
+                                                        clip=clip,
+                                                        min_area=min_area,
+                                                        min_visibility=min_visibility,
+                                                        min_width=min_width,
+                                                        min_height=min_height,
+                                                        check_each_transform=check_each_transform,
                                                         )
                                 )
 
@@ -331,9 +350,74 @@ augment_and_visualize(augconfig=augconfig, img_path=img_path)
 #%%
 from glob import glob
 import os
+from PIL import Image, ImageDraw
+import random
+
+def random_color():
+    return tuple(random.randint(0, 255) for _ in range(3))
+
+
+def polygon_to_bbox(polygon):
+    x_coords = polygon[0::2]
+    y_coords = polygon[1::2]
+    x_min = min(x_coords)
+    y_min = min(y_coords)
+    x_max = max(x_coords)
+    y_max = max(y_coords)
+    width = x_max - x_min
+    height = y_max - y_min
+    return [x_min, y_min, width, height]
+
+def draw_bbox_and_polygons(annotation_path, img_dir, 
+                           visualize_dir="visualize_bbox_and_polygons"
+                           ):
+    os.makedirs(visualize_dir, exist_ok=True)
+    coco = COCO(annotation_path)
+    for id, imginfo in coco.imgs.items():
+        file_name = imginfo["file_name"]
+        imgid = imginfo["id"]
+        ann_ids = coco.getAnnIds(imgIds=imgid)
+        anns = coco.loadAnns(ids=ann_ids)
+        bboxes = [ann["bbox"] for ann in anns]
+        
+        #print(f"all segmentation anns: {len([ann['segmentation'] for ann in anns])}")
+        #print(type(polygons))
+        #print(polygons)
+        #print(f"None empty segmentation ann: {len([ann['segmentation'][0] for ann in anns if ann['segmentation'][0]])}")
+        
+        polygons = [ann["segmentation"][0] for ann in anns]
+        
+        #break
+        image_path = os.path.join(img_dir, file_name)
+        
+        img = Image.open(image_path).convert("RGBA")
+        mask_img = Image.new("RGBA", img.size)
+        draw = ImageDraw.Draw(mask_img)
+        
+        # Draw bounding boxes
+        for bbox, polygon in zip(bboxes, polygons):
+            color = random_color()
+            bbox = [bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]]
+            draw.rectangle(bbox, outline=color, width=2)
+            draw.polygon(polygon, outline=color, fill=color + (100,))
+        
+        blended_img = Image.alpha_composite(img, mask_img)
+        final_img = blended_img.convert("RGB")
+
+        # Draw segmentation masks (polygons)
+        #for polygon in polygons:
+        #    draw.polygon(polygon, outline=color, fill=color + (100,))  # Semi-transparent blue
+
+        # Save the output image
+        output_path = os.path.join(visualize_dir, file_name)  # Replace "visualize_bbox_and_polygons" with your desired output directory path  # Ensure that the directory exists before saving the image  # Example: output_path = "output/image_with_bbox_and_polygons.png"  # Save the image in PNG format  # Example: img.save(output_path, format='PNG')  # Save the image in JPEG format  # Example: img.save(output_path, format='JPEG')  # Save the image in GIF format  # Example: img.save(output_path, format='GIF')  # Save the image in TIFF format  # Example: img.save(output_path, format='TIFF')  # Save the image in WebP format  # Example: img.save(output_path, format='WEBP')
+        final_img.save(output_path, format='PNG') 
+              
+              
 def augment_imgs(augconfig, img_dir, coco_annfilepath, 
                  save_augmented_annotation_as="augmented_annotations.json",
-                 augmented_output_imgdir="augmented_imgs"
+                 augmented_output_imgdir="augmented_imgs",
+                 visualize_augmentations=True,
+                 visualize_dir="visualize_bbox_and_polygons"
                  ):
     os.makedirs(augmented_output_imgdir, exist_ok=True)
     coco = COCO(coco_annfilepath)
@@ -365,7 +449,12 @@ def augment_imgs(augconfig, img_dir, coco_annfilepath,
         aug_masks = augmented_obj["masks"]
         aug_ann_ids = augmented_obj["ann_ids"]
         aug_category_ids = augmented_obj["category_ids"]
-        
+        print(f"number of augmented bboxex: {len(aug_bboxes)}")
+        print(f"number of augmented masks: {len(aug_masks)}")
+        print(f"number NONE empty masks: {len([mask for mask in aug_masks if mask.any()])}")
+        #[print(mask) for mask in aug_masks if mask.any()]
+    
+
         aug_img_height, aug_img_width = aug_image.shape[0], aug_image.shape[1]
         # Calculate the segmentation
         segmentations = []
@@ -376,7 +465,9 @@ def augment_imgs(augconfig, img_dir, coco_annfilepath,
                 contour = contour.flatten().tolist()
                 segmentation.append(contour)
             segmentations.append(segmentation)
-            
+            #[print(mask) for mask in segmentations]
+        print(f"number of polygon segmentations: {len([seg for seg in segmentations if seg])}")
+        
         for img_info in coco.imgs.values():
             if os.path.basename(img_info["file_name"]) == imgname:
                 img_info["width"] = aug_img_width
@@ -389,10 +480,18 @@ def augment_imgs(augconfig, img_dir, coco_annfilepath,
             for anninfo in anns:
                 if aug_annid == anninfo["id"]:
                     anninfo["bbox"] = aug_bbox
-                    anninfo["segmentation"] = aug_segmask
-                    area = aug_bbox[2] * aug_bbox[3]
-                    anninfo["area"] = area
-                    augmented_annotation_info.append(anninfo)
+                    # there are situations where empty segmentation is return
+                    # or there are more masks than bboxes
+                    if aug_segmask:  
+                        anninfo["bbox"] = polygon_to_bbox(aug_segmask[0])
+                        
+                        anninfo["segmentation"] = aug_segmask
+                        area = aug_bbox[2] * aug_bbox[3]
+                        anninfo["area"] = area
+                        augmented_annotation_info.append(anninfo)
+        print(f"After correction \n")
+        print(f"after correction bbox: {len([ann['bbox'] for ann in augmented_annotation_info])}")
+        print(f"after correction segmentation: {len([ann['segmentation'] for ann in augmented_annotation_info])}")
         output_augimg_path = os.path.join(augmented_output_imgdir, imgname)
         Image.fromarray(aug_image).save(output_augimg_path)
                     
@@ -406,19 +505,89 @@ def augment_imgs(augconfig, img_dir, coco_annfilepath,
         
     with open(save_augmented_annotation_as, "w") as outfile:
         json.dump(cocodata, outfile)
-        
-    # TODO. visualization of augmented images
+    if visualize_augmentations:
+        draw_bbox_and_polygons(annotation_path=save_augmented_annotation_as,
+                               img_dir=augmented_output_imgdir,
+                               visualize_dir=visualize_dir
+                               )   
+    # TODO. check why indexerror occurs
+    # possible that no contours are found in the augmented mask sometimes hence empyt segmentation is created
+    # in that case, check for empty segmentation, get the index of it in the list of segmentations
+    # then remove it and also remove the corresponding bbox, ann_id among other to make sure they match
     
-        
+    
+    
+
+
 #%%
 img_dir = "/home/lin/codebase/cv_with_roboflow_data/subset_extract_folder/tomato_fruit"
-augment_imgs(augconfig=augconfig, img_dir=img_dir, coco_annfilepath=coco_path)
+augment_imgs(augconfig=augconfig, img_dir=img_dir, 
+             coco_annfilepath=coco_path
+             )
       
+#%%
+def visualize_segmask(annotation_path, img_dir):
+    #with open(annotation_path,"r") as file:
+        #annot_data = json.load(file)
+    coco = COCO(annotation_path)
+    for id, imginfo in coco.imgs.items():
+        file_name = imginfo["file_name"]
+        imgid = imginfo["id"]
+        ann_ids = coco.getAnnIds(imgIds=imgid)
+        anns = coco.loadAnns(ids=ann_ids)
+        img_path = os.path.join(img_dir, file_name)
+        img = cv2.imread(img_path)
+        mask = np.zeros_like(img)
+        for ann in anns:
+            color = (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255))
+            pts = np.array(ann['segmentation']).reshape(-1, 1, 2).astype(np.int32)
+            cv2.fillPoly(mask, [pts], color)
+            #centroid = np.mean(pts, axis=0)
+        img_masked = cv2.addWeighted(img, 0.7, mask, 0.1, 0)
+        plt.imshow(img_masked)
+        plt.show()
         
-    
+#%%
+
+get_image_and_annotations
+#%%
+visualize_segmask(annotation_path="augmented_annotations.json", 
+                  img_dir="/home/lin/codebase/cv_with_roboflow_data/augmented_imgs"
+                  )   
    
+#%%
+
+    
+#%%
+draw_bbox_and_polygons(annotation_path="augmented_annotations.json", 
+                  img_dir="/home/lin/codebase/cv_with_roboflow_data/augmented_imgs"
+                  )   
+#%%   
+    #     #img = cv2.imread(img_path)
+    #     #mask = np.zeros_like(img)
+    #     for ann in anns:
+    #         color = (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255))
+    #         pts = np.array(ann['segmentation']).reshape(-1, 1, 2).astype(np.int32)
+    #         cv2.fillPoly(mask, [pts], color)
+    #         #centroid = np.mean(pts, axis=0)
+    #     img_masked = cv2.addWeighted(img, 0.7, mask, 0.1, 0)
+    #     plt.imshow(img_masked)
+    #     plt.show()
     
     
+    # # Open the image
+    
+
+    # # Draw bounding boxes
+    # for bbox in bboxes:
+    #     draw.rectangle(bbox, outline="red", width=3)
+
+    # # Draw segmentation masks (polygons)
+    # for polygon in polygons:
+    #     draw.polygon(polygon, outline="blue", fill=(0, 0, 255, 128))  # Semi-transparent blue
+
+    # # Save the output image
+    # img.save(output_path, format='PNG')  
     
 
 
@@ -513,4 +682,36 @@ format_photo(input_path, "reduced_image.jpg")
 
 # %%
 Image.open(input_path).resize((600, 600), Image.LANCZOS).save("reduced_image.jpg")
+# %%
+
+
+#%%
+# Example usage
+image_path = 'augmented_img.png'
+output_path = 'example_output_image.png'
+bboxes = [(50, 50, 150, 150), (200, 200, 300, 300)]  # Example bounding boxes
+polygons = [
+    [0, 0, 0, 100, 100, 100, 100, 0],  # Example polygon 1
+    [200, 200, 200, 300, 300, 300, 300, 200]  # Example polygon 2
+]
+
+draw_bbox_and_polygons(image_path, bboxes, polygons, output_path)
+
+# %%
+def polygon_to_bbox(polygon):
+    x_coords = polygon[0::2]
+    y_coords = polygon[1::2]
+    x_min = min(x_coords)
+    y_min = min(y_coords)
+    x_max = max(x_coords)
+    y_max = max(y_coords)
+    width = x_max - x_min
+    height = y_max - y_min
+    return [x_min, y_min, width, height]
+
+# Example usage
+polygon = [384, 236, 383, 237, 382, 237, 381, 238, 380, 238, 380, 239, 378, 241, 378, 242, 379, 243, 379, 244, 380, 245, 379, 246, 380, 247, 380, 249, 381, 250, 381, 251, 380, 252, 383, 255, 384, 255, 386, 257, 387, 257, 388, 258, 390, 258, 391, 259, 391, 260, 395, 260, 396, 261, 399, 261, 400, 260, 401, 261, 402, 261, 403, 260, 404, 260, 405, 261, 406, 261, 407, 260, 410, 260, 411, 259, 412, 259, 413, 258, 414, 259, 415, 259, 416, 258, 416, 257, 414, 255, 414, 253, 413, 252, 412, 252, 410, 250, 409, 250, 405, 246, 405, 245, 404, 245, 403, 244, 402, 244, 401, 243, 400, 243, 399, 242, 399, 241, 398, 241, 397, 240, 395, 240, 394, 239, 392, 239, 391, 240, 389, 238, 388, 238, 386, 236]
+bbox = polygon_to_bbox(polygon)
+print(bbox)  # Output: [378, 236, 38, 25]
+
 # %%
